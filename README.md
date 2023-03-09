bda7507afbba599a864d10bb4344bc987c5a4fef
Rysaev Damir <freepad@ya.ru>
2023-03-09 20:10:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+1. git
+2. node / yarn
+3. webpack / parcel / rollup / vite
+4. jest / vitest / mocha + chai
+5. linter = eslint + prettier
+6. ci/cd = appveyor / github actions 
+
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+    }),
+  ]
+};
